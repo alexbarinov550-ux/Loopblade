3566812b1ba7fd83a4df4a23f60695489d966ecb
alexbarinov550-ux <alexbarinov550@gmail.com>
2026-05-11 14:25:56 +0300

    Update deepseek_javascript_20260511_6f4f36.js

--- deepseek_javascript_20260511_6f4f36.js
+++ deepseek_javascript_20260511_6f4f36.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-// Разрешаем JSON размером до 10 МБ
 app.use(express.json({ limit: '10mb' }));
 
-// Обработчик загрузки
 app.post('/upload.php', (req, res) => {
     const { secret, code, timestamp } = req.body;
     if (secret !== 'LOOPBLADE_BACKUP') {
